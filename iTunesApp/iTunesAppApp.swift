82656ff56f730e1e264c8d92e4265a8b83caad64
Divyant <divyant.srivastava@fourkites.com>
2024-01-29 14:21:27 +0530

    Initial Commit

---
+++ iTunesApp/iTunesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesAppApp.swift
+//  iTunesApp
+//
+//  Created by Divyant on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iTunesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
